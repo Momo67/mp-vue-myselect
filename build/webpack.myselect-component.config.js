ae64623689f2a6696c16041a18c8d45709f135a0
Maurice Pittet <maurice.pittet@lausanne.ch>
2018-04-20 10:34:57 +0200

    Script for building the myselect component

---
+++ build/webpack.myselect-component.config.js
@@ -0,0 +1,68 @@
+const webpack = require('webpack');
+const path = require('path');
+const utils = require('./utils')
+const APP_ABSOLUTE_PATH = path.join(__dirname, '../src');
+
+function resolve (dir) {
+  return path.join(__dirname, '..', dir)
+}
+
+module.exports = {
+  entry: path.join(APP_ABSOLUTE_PATH, '/components/MySelect.vue'),
+  output: {
+    library: 'my-select',
+    libraryTarget: 'umd',
+    path: path.resolve(__dirname + '/../dist/'),
+    filename: 'my-select.component.js',
+    umdNamedDefine: true
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js' // 'vue/dist/vue.common.js' for webpack 1
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: __dirname,
+        exclude: /node_modules/
+        // include: [resolve('src'), resolve('test')]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('img/[name].[hash:7].[ext]')
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!less!css'
+      },
+    ]
+  },
+  externals: {
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin( {
+      minimize : true,
+      sourceMap : false,
+      mangle: true,
+      compress: {
+        warnings: false
+      }
+    } ),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"testing"'
+      }
+    })
+  ]
+}
